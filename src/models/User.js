b0d384edba974816e603bcbeaf4734cd06b82d12
Leandro-Pinho <leandroessencia@gmail.com>
2024-01-11 19:48:01 -0300

    instalando e incrementando a bcrypt para incriptação da senha para mais segurança

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = mongoose.Schema({
     name: {
@@ -31,6 +32,11 @@ const UserSchema = mongoose.Schema({
 }, {
     timestamps: true,
 });
+
+UserSchema.pre("save", async function (next) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
 
 const User = mongoose.model("User", UserSchema);
 
