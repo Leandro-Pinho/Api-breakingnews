8b5f2b75b0a8b4e6e8c9c249651868b38d3ee71d
Leandro-Pinho <leandroessencia@gmail.com>
2024-01-14 10:08:24 -0300

    adicionado alteração no .env

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from "cors";
 import useRoute from './src/routes/user.route.js';
 import connectDatabase from './src/database/db.js';
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 dotenv.config();
 
+app.use(cors());
 app.use(express.json());
 app.use('/user', useRoute);
 
+// para mostrar no navegador
+app.get("/", (req, res) => res.send("Hello From Express"));
+app.all("*", (req, res) => res.send("That route doesn't exist"));
 
 const username = process.env.BD_USERNAME
 const password = process.env.BD_PASSWORD
