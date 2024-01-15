fab1288238cefa3d6d74a156a5e1a70e4e2c306f
Leandro-Pinho <leandroessencia@gmail.com>
2024-01-15 20:44:51 -0300

    criando o token de autorizaçãço de usuario

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -2,6 +2,25 @@ import { createService, findAllService } from "../service/news.service.js";
 
 export const create = async (req, res) => {
     try {
+        const { authorization } = req.headers;
+        // console.log(authorization)
+
+        if (!authorization) {
+            return res.status(401).json({});
+        }
+
+        const parts = authorization.split(" ");
+
+        const [schema, token] = parts
+
+        if (parts.length !== 2) {
+            return res.status(401)
+        }
+
+        if (schema !== "Bearer") {
+            return res.status(401)
+        }
+
         const { title, text, banner } = req.body;
 
         if (!title || !banner || !text) {
@@ -17,7 +36,7 @@ export const create = async (req, res) => {
             user: { "_id": "659b1d572774da363ecea5a0" }
         })
 
-        res.send(201).json({});
+        res.status(201).json({ message: "News criada com sucesso!" });
 
     } catch (error) {
         res.status(500).send({ message: error.message });
