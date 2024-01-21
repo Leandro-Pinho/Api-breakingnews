23fda0a323b0189a8a5cdd5852a9646a580b6193
Leandro-Pinho <leandroessencia@gmail.com>
2024-01-20 21:58:17 -0300

    paginação no banco de dados

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService } from "../service/news.service.js";
+import { countNews, createService, findAllService } from "../service/news.service.js";
 
 export const create = async (req, res) => {
     try {
@@ -25,11 +25,53 @@ export const create = async (req, res) => {
 }
 
 export const findAll = async (req, res) => {
-    const news = await findAllService();
+
+    // para fazer paginação
+    let { limit, offset } = req.query;
+
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if (!limit) {
+        limit = 5;
+    }
+
+    if (!offset) {
+        offset = 0;
+    }
+
+    const total = await countNews();
+    const currentUrl = req.baseUrl;
+
+    const next = offset + limit;
+    const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previous = limit - offset < 0 ? null : limit - offset;
+    const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null;
+
+    const news = await findAllService(limit, offset);
 
     if (news.length === 0) {
         return res.status(400).send({ message: "Não há noticias cadastradas!" })
     }
 
-    res.send(news)
+    res.send({
+        nextUrl,
+        previousUrl,
+        limit,
+        offset,
+        total,
+
+        results: news.map(item => ({
+            id: item._id,
+            title: item.title,
+            text: item.text,
+            banner: item.banner,
+            likes: item.likes,
+            comments: item.comments,
+            name: item.user.name,
+            username: item.user.username,
+            userAvatar: item.user.avatar,
+        }))
+    })
 }
--- src/service/news.service.js
+++ src/service/news.service.js
@@ -1,4 +1,5 @@
 import News from "../models/News.js";
 
 export const createService = (body) => News.create(body);
-export const findAllService = () => News.find()
+export const findAllService = (limit, offset) => News.find().sort({ _id: -1 }).skip(offset).limit(limit).populate('user');
+export const countNews = () => News.countDocuments();
