69f9f5b19de6396e872ba4937bb8d01f177033fc
Leandro-Pinho <leandroessencia@gmail.com>
2024-01-25 15:30:45 -0300

    criando a busca de noticias por title e a busca de noticias de cada usuario

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { countNews, createService, findAllService, findByIdService, topNewsService } from "../service/news.service.js";
+import { countNews, createService, findAllService, findByIdService, topNewsService, searchByTitleService, byUserService } from "../service/news.service.js";
 
 export const create = async (req, res) => {
     try {
@@ -22,7 +22,7 @@ export const create = async (req, res) => {
     } catch (error) {
         res.status(500).send({ message: error.message });
     }
-}
+};
 
 export const findAll = async (req, res) => {
 
@@ -74,7 +74,7 @@ export const findAll = async (req, res) => {
             userAvatar: item.user.avatar,
         }))
     })
-}
+};
 
 export const topNews = async (req, res) => {
     try {
@@ -100,7 +100,7 @@ export const topNews = async (req, res) => {
     } catch (error) {
         return res.status(401).json({ message: "Não hã registro!" })
     }
-}
+};
 
 export const findById = async (req, res) => {
     try {
@@ -120,9 +120,62 @@ export const findById = async (req, res) => {
                 username: news.user.username,
                 userAvatar: news.user.avatar,
             }
-        })
+        });
 
     } catch (error) {
-        return res.stauts(401).json({ message: error })
+        return res.status(401).json({ message: error })
     }
-}
+};
+
+export const searchByTitle = async (req, res) => {
+    try {
+        const { title } = req.query;
+
+        const news = await searchByTitleService(title);
+
+        if (news.length === 0) {
+            return res.status(400).json({ message: "There are no news with this title" });
+        }
+
+        return res.json({
+            results: news.map((item) => ({
+                id: item._id,
+                title: item.title,
+                text: item.text,
+                banner: item.banner,
+                likes: item.likes,
+                comments: item.comments,
+                name: item.user.name,
+                username: item.user.username,
+                userAvatar: item.user.avatar,
+            }))
+        });
+
+    } catch (error) {
+        return res.status(401).json({ message: error })
+    }
+};
+
+export const byUser = async (req, res) => {
+    try {
+        const id = req.userId;
+        const news = await byUserService(id);
+
+        return res.json({
+            results: news.map((item) => ({
+                id: item._id,
+                title: item.title,
+                text: item.text,
+                banner: item.banner,
+                likes: item.likes,
+                comments: item.comments,
+                name: item.user.name,
+                username: item.user.username,
+                userAvatar: item.user.avatar,
+            }))
+        });
+
+    } catch (error) {
+        return res.status(401).json({ message: error })
+    }
+};
--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middlewares.js";
-import { findAll, create, topNews, findById } from "../controllers/news.controller.js";
+import { findAll, create, topNews, findById, searchByTitle, byUser } from "../controllers/news.controller.js";
 
 const route = Router();
 
-route.post("/", authMiddleware, create)
-route.get("/", findAll)
-route.get("/top", topNews)
-route.get("/:id", findById);
+route.post("/", authMiddleware, create);
+route.get("/", findAll);
+route.get("/top", topNews);
+route.get("/search", searchByTitle);
+route.get("/byUser", authMiddleware, byUser);
+
+route.get("/:id", authMiddleware, findById);
 
 export default route;
