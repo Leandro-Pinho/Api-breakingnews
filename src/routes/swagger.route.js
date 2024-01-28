488557ebc86833a7349ef98c9e50cdb853f9ea71
Leandro-Pinho <leandroessencia@gmail.com>
2024-01-27 21:47:00 -0300

    continuando criando o swagger

--- src/routes/swagger.route.js
+++ src/routes/swagger.route.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "../../swagger.json"
+import swaggerDocument from "../../swagger.json" assert { type: "json" };
 
 const route = Router();
 
